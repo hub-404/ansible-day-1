812ff028315d198cadd57913d6fe3cb2988f47fd
Veros Kaplan <VerosK@noreply.users.github.com>
2016-11-02 23:08:59 +0100

    Updated initial script

--- 00-create_hosts/00-start-droplets.sh
+++ 00-create_hosts/00-start-droplets.sh
@@ -4,7 +4,7 @@ INVENTORY_PATH=$( dirname $0 )
 
 #echo '[digitalocean]' > ${INVENTORY_PATH}/hosts.do
 
-ansible-playbook -i hosts.setup 01-start-droplets.yml -vv -e "inventory_path=${INVENTORY_PATH}" 
+#ansible-playbook -i hosts.setup 01-start-droplets.yml -vv -e "inventory_path=${INVENTORY_PATH}" 
 
 ansible-playbook -i hosts.do 02-setup-droplets.yml -vv
 
