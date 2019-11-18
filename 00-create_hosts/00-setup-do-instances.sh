9d303c7c27dc95629406cabbd3ea191749f3a2fd
Veros Kaplan <VerosK@users.noreply.github.com>
2019-11-18 13:26:06 +0100

    Use one ansible-playbook command instead of sequential call

--- 00-create_hosts/00-setup-do-instances.sh
+++ 00-create_hosts/00-setup-do-instances.sh
@@ -5,8 +5,9 @@ if ! [ -f hosts ] ; then
 	exit
 fi
 
+export ANSIBLE_HOST_KEY_CHECKING=False
 env ANSIBLE_HOST_KEY_CHECKING=False ansible  -i hosts -u root -m ping -f 10  -l ansible-training ansible-training -v
 
-ansible-playbook -i hosts -l ansible-training 02-setup-droplets.yml -v
+ansible-playbook -i hosts -l ansible-training 02-setup-droplets.yml 03-setup-droplets.yml -v
 
 ansible-playbook -i hosts -l ansible-training 03-setup-droplets.yml -v
--- 00-create_hosts/00-setup-scw-instances.sh
+++ 00-create_hosts/00-setup-scw-instances.sh
@@ -6,7 +6,7 @@ INVENTORY_PATH=$( dirname $0 )
 
 #ansible-playbook -i hosts.setup 01-start-droplets.yml -vv -e "inventory_path=${INVENTORY_PATH}" 
 
-env ANSIBLE_HOST_KEY_CHECKING=False ansible  -i ~/bin/scaleway_inventory -u root -m ping -f 1  -l ansible-training ansible-training -v
+env ANSIBLE_HOST_KEY_CHECKING=False ansible  -i ~/bin/scaleway_inventory -u root -m ping -f 10  -l ansible-training ansible-training -v
 
 ansible-playbook -i ~/bin/scaleway_inventory -l ansible-training 02-setup-droplets.yml -v
 
