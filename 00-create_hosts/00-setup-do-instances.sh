48a815bd5316712eaf3f65205f9daf0cb127af2e
Věroš Kaplan <veros.kaplan@wftech.cz>
2022-05-10 07:13:38 +0200

    chore: cleanup setup scripts

--- 00-create_hosts/00-setup-do-instances.sh
+++ 00-create_hosts/00-setup-do-instances.sh
@@ -8,6 +8,5 @@ fi
 export ANSIBLE_HOST_KEY_CHECKING=False
 env ANSIBLE_HOST_KEY_CHECKING=False ansible  -i hosts -u root -m ping -f 10  -l ansible-training ansible-training -v
 
-ansible-playbook -i hosts -l ansible-training 02-setup-droplets.yml 03-setup-droplets.yml -v
 
 ansible-playbook -i hosts -l ansible-training 03-setup-droplets.yml -v
